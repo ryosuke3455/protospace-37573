5972f3d3ce709ebbfe56c426443c2076ebf19b51
ryosuke3455 <ryosuke.m2ds7@softbank.ne.jp>
2022-02-17 12:49:59 +0900

    挙動修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :syousai_prototype, except: [:index, :new, :create]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.includes(:user)
